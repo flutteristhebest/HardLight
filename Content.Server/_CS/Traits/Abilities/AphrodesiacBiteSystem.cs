de4ad71480b3cfd97b13d0c2d593446ec27a81cb
Lily Autumn <45874270+AutumnalModding@users.noreply.github.com>
2026-04-04 10:10:50 +1300

    Aphrodesiac Bite Trait~ (#1106)

---
+++ Content.Server/_CS/Traits/Abilities/AphrodesiacBiteSystem.cs
@@ -0,0 +1,59 @@
+using Content.Server._Common.Consent;
+using Content.Server.Body.Components;
+using Content.Server.Body.Systems;
+using Content.Shared._CS.Traits.Abilities;
+using Content.Shared.Actions;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.Popups;
+using Robust.Shared.Audio.Systems;
+
+namespace Content.Server._CS.Traits.Abilities;
+
+public sealed class AphrodesiacBiteSystem : EntitySystem
+{
+    [Dependency] private readonly ConsentSystem _consent = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+    [Dependency] private readonly BloodstreamSystem _bloodstream = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AphrodesiacBiteEvent>(OnBite);
+        SubscribeLocalEvent<AphrodesiacBiteComponent, ComponentInit>(OnInit);
+    }
+
+    public void OnInit(EntityUid uid, AphrodesiacBiteComponent component, ComponentInit args)
+    {
+        _actions.AddAction(uid, ref component.ActionEntity, component.Action, uid);
+    }
+
+    public void OnBite(AphrodesiacBiteEvent ev)
+    {
+        if (ev.Handled)
+            return;
+
+        TryInject(ev.Target, ev.Performer);
+    }
+
+    public void TryInject(EntityUid target, EntityUid user)
+    {
+        if (!TryComp<BloodstreamComponent>(target, out var bloodstream))
+            return;
+
+        if (!TryComp<AphrodesiacBiteComponent>(user, out var bite))
+            return;
+
+        if (bite.RequiresConsent && !_consent.HasConsent(target, bite.ConsentToggleId))
+        {
+            _popup.PopupEntity(Loc.GetString("aphrodesiac-no-consent", ("target", target)), user, PopupType.LargeCaution);
+            return;
+        }
+
+        var solution = new Solution(bite.Reagent, bite.Amount);
+        if (_bloodstream.TryAddToChemicals(target, solution, bloodstream))
+            _audio.PlayPvs(bite.Sound, user);
+    }
+}
