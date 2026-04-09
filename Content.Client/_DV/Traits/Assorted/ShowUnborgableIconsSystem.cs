33a1f39c028b4a33248ec897fb8087cf5d20efe8
fenndragon <fenndragon@gmail.com>
2026-04-08 19:05:27 -0600

    Revert "Update ShowUnborgableIconsSystem.cs"
    This reverts commit e32a4745023e58937e0ba781cc3dca04dc205fe4.

--- Content.Client/_DV/Traits/Assorted/ShowUnborgableIconsSystem.cs
+++ Content.Client/_DV/Traits/Assorted/ShowUnborgableIconsSystem.cs
@@ -1,6 +1,5 @@
 using Content.Client.Overlays;
 using Content.Shared._DV.Traits.Assorted;
-using Content.Shared.Inventory.Events;
 using Content.Shared.Overlays;
 using Content.Shared.Silicons.Borgs.Components;
 using Content.Shared.StatusIcon;
